478d2b65ef272e72d4b53e261821c17562f2b4aa
ryanremington84 <ryanremington84@gmail.com>
2026-04-07 19:55:32 -0400

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,4 @@
 "use client";
-
 import { useEffect, useState } from "react";
 import Link from "next/link";
 import Productivity from "@/components/landing/productivity";
@@ -7,31 +6,25 @@ import { HeroSection } from "@/components/landing/hero";
 import { features } from "@/db/features";
 import { ChevronRight } from "lucide-react";
 import Features from "@/components/landing/features";
-import Explainer from "@/components/landing/explainer";
 import CTA from "@/components/landing/cta";
 import Footer from "@/components/landing/footer";
 import Navbar from "@/components/landing/navbar";
 import Agents from "@/components/landing/agents";
+import QuantonDashboard from "@/components/landing/QuantonDashboard";
 
 export default function Home() {
   const [isScrolled, setIsScrolled] = useState(false);
-
   useEffect(() => {
     const handleScroll = () => {
       const triggerPoint = window.innerHeight * 0.3;
       setIsScrolled(window.scrollY > triggerPoint);
     };
-
     handleScroll();
     window.addEventListener("scroll", handleScroll);
     return () => window.removeEventListener("scroll", handleScroll);
   }, []);
-
-
-
   return (
     <div className="w-full min-h-[200vh]">
-
       {/* Background */}
       <div className="fixed z-0 bg-slate-400/10 w-[500px] h-[500px] blur-3xl top-40 left-40 animate-bounce"></div>
       <div className="fixed z-0 bg-slate-400/10 w-[500px] h-[500px] blur-3xl bottom-10 right-40 animate-bounce"></div>
@@ -39,13 +32,12 @@ export default function Home() {
       <div className="fixed z-0 bg-slate-400/10 w-[500px] h-[500px] blur-3xl -bottom-80 left-80 animate-bounce"></div>
       {/* Navbar */}
       <Navbar isScrolled={isScrolled} />
-
       {/* Hero */}
       <HeroSection />
-      <Productivity />
-      <Agents/>
       <Features />
-      <Explainer />
+      <Productivity />
+      <Agents />
+      <QuantonDashboard />
       <CTA />
       <Footer />
     </div>
